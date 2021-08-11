2a17f10022a55c6aa05f2ac5d1ebee1cfaa578f6
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-08-11 12:58:37 +0300

    chore: remove quotes for better readability

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
   root: true,
   rules: {
     '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/consistent-type-definitions': ['off'],
     '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/member-ordering': ['off'],
     '@typescript-eslint/naming-convention': ['off'],
@@ -39,8 +40,8 @@ module.exports = {
     '@typescript-eslint/prefer-readonly-parameter-types': ['off'],
     '@typescript-eslint/quotes': ['error', 'backtick'],
     '@typescript-eslint/type-annotation-spacing': ['error', {
-      'before': true,
-      'after': true,
+      before: true,
+      after: true,
     }],
     'arrow-body-style': ['off'],
     'comma-dangle': ['off'],
@@ -51,43 +52,53 @@ module.exports = {
       'error', {
         max: 500,
         skipBlankLines: true,
-        skipComments: true
+        skipComments: true,
       }
     ],
     'no-ternary': ['off'],
     'object-curly-newline': [
       'error', {
-      'ObjectExpression': 'always',
-      'ObjectPattern': { 'multiline': true },
-      'ImportDeclaration': { 'multiline': true, 'minProperties': 2 },
-      'ExportDeclaration': { 'multiline': true, 'minProperties': 2 }
+      ObjectExpression: 'always',
+      ObjectPattern: {
+        multiline: true
+      },
+      ImportDeclaration: {
+        multiline: true,
+        minProperties: 2 },
+      ExportDeclaration: {
+        multiline: true,
+        minProperties: 2 }
     }],
     'one-var': ['off'],
     'padded-blocks': ['off'],
     'padding-line-between-statements': [
       'error', {
-        'blankLine': 'always',
-        'prev': '*',
-        'next': 'return'
+        blankLine: 'always',
+        prev: '*',
+        next: 'return'
       },
     ],
     'react/function-component-definition': [ 2, {
-      'namedComponents': 'arrow-function',
-      'unnamedComponents': 'arrow-function'
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function'
     }],
     'react/jsx-curly-brace-presence': [2, {
-      'props': 'always',
-      'children': 'always'
+      props: 'always',
+      children: 'always'
     }],
     'react/jsx-filename-extension': ['off'],
     'react/jsx-indent': ['off'],
     'react/jsx-indent-props': [2, 2],
     'react/jsx-max-props-per-line': [2, {
-      'maximum': 2,
-      'when': 'always'
+      maximum: 2,
+      when: 'always'
     }],
-    'react/jsx-newline': [2, { 'prevent': true }],
-    'react/jsx-one-expression-per-line': [2, {allow: 'single-child'}],
+    'react/jsx-newline': [2, {
+      prevent: true
+    }],
+    'react/jsx-one-expression-per-line': [2, {
+      allow: 'single-child'
+    }],
     'react/no-multi-comp': ['off'],
     'react/prop-types': ['off'],
     'react/require-default-props': ['off'],
